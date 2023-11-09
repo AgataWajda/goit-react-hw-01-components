2ea229486f1c048395d6e58cda3f525eb973a6e1
Agata Wajda <agawaj98@gmail.com>
2023-11-09 01:23:53 +0100

    init profile

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Profile } from 'components/Profile';
+import users from 'data/user.json';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile
+        avatar={users.user.avatar}
+        username={users.user.username}
+        tag={users.user.tag}
+        location={users.user.location}
+        followers={users.user.stats.followers}
+        views={users.user.stats.views}
+        likes={users.user.stats.likes}
+      />
+    </App>
   </React.StrictMode>
 );
