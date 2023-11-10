d0de72e0f55ac74bf7523c8a930687311058bb0d
Agata Wajda <agawaj98@gmail.com>
2023-11-10 02:07:03 +0100

    init styles in profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
+import { Profile } from 'components/Profile';
+import users from 'data/user.json';
+
 export const App = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Profile
+        avatar={users.user.avatar}
+        username={users.user.username}
+        tag={users.user.tag}
+        location={users.user.location}
+        followers={users.user.stats.followers}
+        views={users.user.stats.views}
+        likes={users.user.stats.likes}
+      />
+    </div>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { Profile } from 'components/Profile';
-import users from 'data/user.json';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App>
-      <Profile
-        avatar={users.user.avatar}
-        username={users.user.username}
-        tag={users.user.tag}
-        location={users.user.location}
-        followers={users.user.stats.followers}
-        views={users.user.stats.views}
-        likes={users.user.stats.likes}
-      />
-    </App>
+    <App></App>
   </React.StrictMode>
 );
